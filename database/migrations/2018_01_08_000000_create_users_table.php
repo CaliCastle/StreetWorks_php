9838d13474e9e92ca65e320c2a21f2dac817b4c1
Cali Castle <cali@calicastle.com>
2018-01-17 17:22:16 -0800

    Make `username` unique

--- database/migrations/2018_01_08_000000_create_users_table.php
+++ database/migrations/2018_01_08_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->uuid('id')->unique();
             $table->string('first_name');
             $table->string('last_name');
-            $table->string('username');
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->string('password');
             $table->smallInteger('status')->default(1);
